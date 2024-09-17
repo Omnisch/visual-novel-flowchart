c52374f7478219c88cbe7aec9877f4b183037992
Omnisch <fleurdeln@gmail.com>
2024-09-17 21:00:00 +0800

    import file explorer interface

--- Assets/Scripts/IO.cs
+++ Assets/Scripts/IO.cs
@@ -10,15 +10,24 @@ namespace Omnis.BranchTracker
         #endregion
 
         #region Fields
+        private string savePath;
+        private string loadPath;
         #endregion
 
         #region Interfaces
+        public void LoadFilePath()
+        {
+            var bp = new AnotherFileBrowser.Windows.BrowserProperties
+            {
+                filter = "Visual novel flowchart files (*.txt, *.vnf) | *.txt; *.vnf",
+                filterIndex = 0
+            };
+
+            new AnotherFileBrowser.Windows.FileBrowser().OpenFileBrowser(bp, path => loadPath = path);
+        }
         #endregion
 
         #region Functions
-        #endregion
-
-        #region Unity Methods
         #endregion
     }
 }
